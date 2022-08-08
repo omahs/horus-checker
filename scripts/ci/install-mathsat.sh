2b688b9337af21b5dd032a93341795c25bf81055
Artem Yurchenko <artemyurchenko@zoho.com>
2022-08-08 16:20:42 +0400

    WIP

--- scripts/ci/install-mathsat.sh
+++ scripts/ci/install-mathsat.sh
@@ -2,7 +2,7 @@
 
 set -eu
 
-NAME=mathsat-5.6.8-linux-x86_64-reentrant
+NAME=mathsat-5.6.7-linux-x86_64
 TEMP=$(mktemp)
 curl -L -o "${TEMP}" "https://mathsat.fbk.eu/download.php?file=${NAME}.tar.gz"
 tar -zxvf "${TEMP}" "${NAME}/bin/mathsat"
